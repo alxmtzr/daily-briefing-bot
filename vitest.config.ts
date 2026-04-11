2f881333b8abeafdb89eb1136335ed4797b3bf6a
I762886 <alexander.metzler@sap.com>
2026-04-11 12:18:14 +0200

    add coverage rpoerter

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
         "coverage": {
             "provider": "v8",
             "include": ["src/**/*.ts"],
+            "reporter": ["text", "json-summary"],
             "thresholds": {
                 "branches": 80,
                 "functions": 80,
